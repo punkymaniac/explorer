c56c209682452a7182cec949532079b0d78a3e4d
punkymaniac <droger@student.42.fr>
2018-10-12 16:04:28 +0200

    Add example file
     A simple use of the module

---
+++ example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import explorer
+
+extension = ['.py']
+
+
+listFile = explorer.search_file("./", hide=True)
+print(listFile)
+print("\n")
+
+listFile = explorer.search_file("./", extension, False)
+print(listFile)
+print("\n")
+
+listDir = explorer.search_dir("./", hide=False)
+print(listDir)
+
+
